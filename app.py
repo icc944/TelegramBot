2f5caf9d3645e26be84b3b2f1dafd5ef50970394
Issac Corona Córdoba <98198234+YzackZack476@users.noreply.github.com>
2022-11-15 18:27:44 -0600

    Add new features

--- app.py
+++ app.py
@@ -6,31 +6,39 @@ from package.library.config import config, TOKEN
 from package.library import utilities as utl
 import telebot
 
-bot = telebot.TeleBot(TOKEN)
+mybot = telebot.TeleBot(TOKEN)
 
 
 
-@bot.message_handler(commands=['start'])
+@mybot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "¡Hola que tranza!, ¿Que puedo hacer por ti?")
+    mybot.reply_to(message, "¡Hola que tranza!, ¿Que puedo hacer por ti?");
 
-@bot.message_handler(commands=['help'])
+@mybot.message_handler(commands=['help'])
 def send_help(message):
-    bot.reply_to(message, """
+    mybot.reply_to(message, """
     Puedes usar:\n
     \help: Para ayuda\n
     \hora: Para que te diga la hora\n
     Nomas eso se hacer por ahora unu
-    """)
+    """);
 
-@bot.message_handler(commands=['hora'])
+@mybot.message_handler(commands=['hora'])
 def send_hora(message):
     # Fecha
     now = utl.get_now()
-    bot.reply_to(message, now)
+    mybot.reply_to(message, now);
 
-@bot.message_handler(func=lambda message: True)
+@mybot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    mybot.reply_to(message, message.text);
 
-bot.polling()
+# Saber que chat envio el mensaje y enviarle un archivo excel
+@mybot.message_handler(commands=['send_data'])
+def send_data(message):
+    chat_id = message.chat.id;
+    data = open('c:/user/carpeta/data.xlsx','rb')
+    mybot.send_document(chat_id, data);
+    mybot.reply_to(message, "¡Listo!");
+    
+bot.polling();
